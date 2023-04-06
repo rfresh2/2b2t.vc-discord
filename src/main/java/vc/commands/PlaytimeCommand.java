a7ca2b5cce9b9ac412a82755d72d4ed2ee29eff5
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-04-06 02:36:20 -0700

    setup swagger codegen for http client

--- src/main/java/vc/commands/PlaytimeCommand.java
+++ src/main/java/vc/commands/PlaytimeCommand.java
@@ -3,11 +3,16 @@ package vc.commands;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import discord4j.core.object.command.ApplicationCommandInteractionOption;
 import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
+import io.swagger.client.api.PlaytimeApi;
+import io.swagger.client.model.PlaytimeResponse;
 import reactor.core.publisher.Mono;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public class PlaytimeCommand implements SlashCommand {
+
+    private final PlaytimeApi playtimeApi = new PlaytimeApi();
 
     @Override
     public String getName() {
@@ -27,6 +32,9 @@ public class PlaytimeCommand implements SlashCommand {
         //  validate playerName is correct regex
         //  for playerName we need to resolve the player's profile to UUID through mojang API
         //  implement api.2b2t.vc interaction class(es)
+
+        PlaytimeResponse playtime = playtimeApi.playtime(UUID.fromString("572e683c-888a-4a0d-bc10-5d9cfa76d892"));
+        Integer playtimeSeconds = playtime.getPlaytimeSeconds();
 
         return null;
     }
