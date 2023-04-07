71297c7f17edd453d3415d1ef2df90ebf12a7d85
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-04-06 20:41:45 -0700

    log command events and write logs to file

--- src/main/java/vc/listeners/SlashCommandListener.java
+++ src/main/java/vc/listeners/SlashCommandListener.java
@@ -2,18 +2,26 @@ package vc.listeners;
 
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.object.command.ApplicationCommandInteraction;
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import vc.commands.SlashCommand;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public class SlashCommandListener {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger("CommandListener");
     private final Collection<SlashCommand> commands;
 
     public SlashCommandListener(List<SlashCommand> slashCommands, GatewayDiscordClient client) {
@@ -30,7 +38,24 @@ public class SlashCommandListener {
             .filter(command -> command.getName().equals(event.getCommandName()))
             //Get the first (and only) item in the flux that matches our filter
             .next()
+            .doOnSuccess(command -> logMessage(command, event))
             //Have our command class handle all logic related to its specific command.
             .flatMap(command -> command.handle(event)));
+    }
+
+    private void logMessage(SlashCommand command, final ChatInputInteractionEvent event) {
+        Optional<String> username = event.getInteraction().getMember()
+                .map(User::getUsername);
+        Optional<String> discrim = event.getInteraction().getMember()
+                .map(User::getDiscriminator);
+        String dataOptions = event.getInteraction().getCommandInteraction()
+                .map(ApplicationCommandInteraction::getOptions)
+                .orElse(Collections.emptyList())
+                .stream()
+                .map(s -> s.getName() + s.getValue().map(v -> ": " + v.asString()).orElse(""))
+                .collect(Collectors.joining(", "));
+        LOGGER.info(username.orElse("?") + "#" + discrim.orElse("?")
+                + " executed command: " + command.getName()
+                + (dataOptions.length() > 0 ? " with options: " + dataOptions : ""));
     }
 }
