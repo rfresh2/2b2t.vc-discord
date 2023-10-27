8309c9ccd09ce2528f66476088f2558b6776e7db
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-10-27 00:38:21 -0700

    valid username check

--- src/main/java/vc/util/PlayerLookup.java
+++ src/main/java/vc/util/PlayerLookup.java
@@ -1,5 +1,6 @@
 package vc.util;
 
+import org.slf4j.Logger;
 import vc.swagger.minetools_api.handler.UuidApi;
 import vc.swagger.minetools_api.model.UUIDAndPlayerName;
 
@@ -11,11 +12,16 @@ import java.util.UUID;
 
 public class PlayerLookup {
     private final UuidApi uuidApi = new UuidApi();
+    private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger("PlayerLookup");
 
     public record PlayerIdentity(UUID uuid, String playerName) { }
 
     public Optional<PlayerIdentity> getPlayerIdentity(final String playerName) {
-        final UUIDAndPlayerName uuidAndPlayerName = uuidApi.getUUIDAndPlayerName(playerName);
+        if (!validPlayerName(playerName)) {
+            LOGGER.info("Invalid playerName: {}", playerName);
+            return Optional.empty();
+        }
+        final UUIDAndPlayerName uuidAndPlayerName = uuidApi.getUUIDAndPlayerName(playerName.trim());
         if (uuidAndPlayerName == null) return Optional.empty();
         if (uuidAndPlayerName.getStatus() != UUIDAndPlayerName.StatusEnum.OK) return Optional.empty();
         return Optional.of(new PlayerIdentity(UUID.fromString(uuidAndPlayerName
@@ -34,5 +40,9 @@ public class PlayerLookup {
         } catch (MalformedURLException e) {
             throw new UncheckedIOException(e);
         }
+    }
+
+    private boolean validPlayerName(String username) {
+        return username.matches("[a-zA-Z0-9_]{3,16}");
     }
 }
