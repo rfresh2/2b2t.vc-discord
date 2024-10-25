ab50ec7384b5fc665fa32a72f4e9301aa026d116
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-10-24 19:59:56 -0700

    simplify command lookup to a map

--- src/main/java/vc/listeners/SlashCommandListener.java
+++ src/main/java/vc/listeners/SlashCommandListener.java
@@ -8,40 +8,37 @@ import discord4j.core.object.entity.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import vc.commands.SlashCommand;
 import vc.config.GuildConfigManager;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
 public class SlashCommandListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger("CommandListener");
-    private final Collection<SlashCommand> commands;
+    private final Map<String, SlashCommand> commandMap;
     private final GuildConfigManager guildConfigManager;
 
     public SlashCommandListener(List<SlashCommand> slashCommands, GatewayDiscordClient client, final GuildConfigManager guildConfigManager) {
-        this.commands = slashCommands;
+        this.commandMap = slashCommands.stream().collect(Collectors.toMap(SlashCommand::getName, c -> c));
         this.guildConfigManager = guildConfigManager;
         client.on(ChatInputInteractionEvent.class, this::handle).subscribeOn(Schedulers.boundedElastic()).subscribe();
     }
 
     public Mono<Message> handle(ChatInputInteractionEvent event) {
-        //Convert our list to a flux that we can iterate through
-        return event.deferReply().then(Flux.fromIterable(commands)
-            //Filter out all commands that don't match the name this event is for
-            .filter(command -> command.getName().equals(event.getCommandName()))
-            //Get the first (and only) item in the flux that matches our filter
-            .next()
-            .doOnSuccess(command -> logMessage(command, event))
-            //Have our command class handle all logic related to its specific command.
-            .flatMap(command -> command.handle(event)));
+        var command = commandMap.get(event.getCommandName());
+        if (command == null) {
+            return event.reply("Command not found").dematerialize();
+        }
+        return event.deferReply()
+            .doOnSuccess(msg -> logMessage(command, event))
+            .then(Mono.defer(() -> command.handle(event)));
     }
 
     private void logMessage(SlashCommand command, final ChatInputInteractionEvent event) {
