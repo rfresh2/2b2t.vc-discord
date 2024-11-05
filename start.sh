90f45d218997c408ecb40d33225af79c59d0a740
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-11-05 08:52:23 +0000

    increase xmx

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-mvn clean package spring-boot:repackage -DskipTests
-java -Xmx100m -jar target/2b2t.vc-discord-1.0.jar
+./mvnw clean package spring-boot:repackage -DskipTests
+java -Xmx250m -jar target/2b2t.vc-discord-1.0.jar
