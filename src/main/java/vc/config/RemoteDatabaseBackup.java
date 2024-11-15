b0ea27ae8cfc23469b1a9106964990b7cc3f3c2d
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-11-14 20:31:20 -0800

    replace aws sdk client

--- src/main/java/vc/config/RemoteDatabaseBackup.java
+++ src/main/java/vc/config/RemoteDatabaseBackup.java
@@ -1,24 +1,22 @@
 package vc.config;
 
+import io.minio.*;
+import io.minio.messages.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.S3Object;
 
-import java.net.URI;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.StreamSupport;
 
 @Component
 public class RemoteDatabaseBackup {
     private static final Logger LOGGER = LoggerFactory.getLogger("RemoteDatabaseBackup");
-    private final S3Client s3Client;
     private final String bucketName;
+    private final MinioClient minioClient;
 
     public RemoteDatabaseBackup(
         @Value("${BUCKET_URL}") final String bucketUrl,
@@ -27,16 +25,21 @@ public class RemoteDatabaseBackup {
         @Value("${BUCKET_NAME}") final String bucketName
     ) {
         this.bucketName = bucketName;
-        s3Client = S3Client.builder()
-            .endpointOverride(URI.create(bucketUrl))
-            .region(Region.of("auto"))
-            .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(awsAccessKeyId, awsSecretAccessKey)))
+        minioClient = MinioClient.builder()
+            .endpoint(bucketUrl)
+            .credentials(awsAccessKeyId, awsSecretAccessKey)
             .build();
     }
 
     public void uploadDatabaseBackup(final String backupPath) {
-        try {
-            s3Client.putObject(builder -> builder.bucket(bucketName).key(backupPath).build(), Paths.get(backupPath));
+        Path path = Paths.get(backupPath);
+        try (var fileStream = Files.newInputStream(path)) {
+            minioClient.putObject(
+                PutObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(backupPath)
+                    .stream(fileStream, Files.size(path), -1)
+                    .build());
             LOGGER.info("Uploaded database backup: {}", backupPath);
         } catch (final Exception e) {
             LOGGER.error("Error uploading database backup: {}", backupPath, e);
@@ -55,14 +58,17 @@ public class RemoteDatabaseBackup {
 
     public String findLatestDatabaseBackup() {
         try {
-            var backupPath = s3Client.listObjects(builder ->
-                                                      builder.bucket(bucketName)
-                                                          .build())
-                .contents()
-                .stream()
-                .distinct()
+            var backupPath = StreamSupport.stream(
+                minioClient.listObjects(
+                    ListObjectsArgs.builder()
+                        .bucket(bucketName)
+                        .prefix("backups/guild-config-backup-")
+                        .build())
+                    .spliterator(), false)
+                .map(RemoteDatabaseBackup::retrieveS3ItemData)
+                .filter(i -> !i.isDir())
                 .sorted((o1, o2) -> o2.lastModified().compareTo(o1.lastModified()))
-                .map(S3Object::key)
+                .map(Item::objectName)
                 .findFirst()
                 .orElseThrow();
             LOGGER.info("Found latest database backup: {}", backupPath);
@@ -73,11 +79,26 @@ public class RemoteDatabaseBackup {
         }
     }
 
+    private static Item retrieveS3ItemData(Result<Item> r) {
+        try {
+            return r.get();
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public void downloadDatabaseBackup(final String backupPath) {
         try {
             var out = Paths.get("guild-config.db");
             Files.deleteIfExists(out);
-            s3Client.getObject(builder -> builder.bucket(bucketName).key(backupPath).build(), out);
+            var data = minioClient.getObject(
+                GetObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(backupPath)
+                    .build());
+            try (var outStream = Files.newOutputStream(out)) {
+                outStream.write(data.readAllBytes());
+            }
             LOGGER.info("Downloaded database backup: {}", backupPath);
         } catch (final Exception e) {
             LOGGER.error("Error downloading database backup: {}", backupPath, e);
