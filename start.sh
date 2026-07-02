10e5c14fea43eace5a2bb4f86eb110e59e434c61
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-07-01 23:14:57 -0700

    update start script jvm args

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 ./mvnw clean package spring-boot:repackage -DskipTests
-java -Xmx250m -jar target/2b2t.vc-discord-1.0.jar
+java -Xmx550m -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:+PerfDisableSharedMem -XX:+UseCompactObjectHeaders --sun-misc-unsafe-memory-access=allow --enable-native-access=ALL-UNNAMED -jar target/2b2t.vc-discord-1.0.jar
